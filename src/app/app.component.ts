00101c82e7ef94cdab23c8395ceb84e43bd5875d
JohnFreddyTutistar <johnfre.157@gmail.com>
2024-09-19 11:49:37 -0500

    app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  @Output() toggleSidenav = new EventEmitter();
 
   sideNavOpened = true;
-  title = 'EduTrackPro';
+
+  constructor(private breakPointObserver: BreakpointObserver, private router: Router,){
+    const isSmallScreen = this.breakPointObserver.isMatched("(max-width: 799px)")
+
+    if(isSmallScreen){
+      this.sideNavOpened = !this.sideNavOpened;
+    }
+  }
+  ngOnInit(): void {
+    
+  }
+
+  changeOrigin(path: string){
+    this.toggleSidenav.emit()
+    this.router.navigateByUrl(path);
+
+  }
+
+  
 }
