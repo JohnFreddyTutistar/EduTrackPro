ebcc169fb2846658bf6748a204d1eb73fa3a9b5a
JohnFreddyTutistar <johnfre.157@gmail.com>
2025-04-30 09:23:40 -0500

    terminando interfaz de inscripción, falta incluir reactividad en el formulario

--- src/app/pages/inscriptions/inscriptions.component.ts
+++ src/app/pages/inscriptions/inscriptions.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { EnumsService } from 'src/app/services/enums.service';
 
 @Component({
   selector: 'app-inscriptions',
   templateUrl: './inscriptions.component.html',
-  styleUrls: ['./inscriptions.component.scss']
+  styleUrls: ['./inscriptions.component.scss'],
 })
 export class InscriptionsComponent implements OnInit {
+  identificationType!: any[];
 
-  constructor() { }
+  constructor(public enumService: EnumsService) {}
 
   ngOnInit(): void {
+    this.identificationType = this.enumService.getIdentificationType();
   }
-
 }
